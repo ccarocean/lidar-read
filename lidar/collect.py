a226442cc0dc30cb1e7379667c47afe81cac7067
Adam Dodge <adam.dodge@colorado.edu>
2019-06-24 13:23:07 -0600

    debugging

--- lidar/collect.py
+++ lidar/collect.py
@@ -1,6 +1,25 @@
 import datetime as dt
 import time
-from .connect import Lidar
+import smbus
+
+
+class Lidar:
+    def __init__(self, address):
+        self.bus = smbus.SMBus(1)
+        self.address = address
+        self.bus.write_byte_data(self.address, 0x02, 0xff)
+        self.bus.write_byte_data(self.address, 0x04, 0x88)
+        self.bus.write_byte_data(self.address, 0x00, 0x04)
+        self.countlist = []
+
+    def read_meas(self):
+        count = 0
+        self.bus.write_byte_data(self.address, 0x00, 0x04)
+        while self.bus.read_byte_data(self.address, 0x01) & 0x01:
+            count += 1
+        self.countlist.append(count)
+        data = self.bus.read_i2c_block_data(self.address, 0x0f, 2)
+        return (data[0] << 8) + data[1]
 
 
 def collect_data(q):
@@ -8,5 +27,6 @@ def collect_data(q):
     while True:
         t = dt.datetime.utcnow()
         meas = lid.read_meas()
+        print(meas)
         q.put_nowait((t, meas))
         time.sleep(max((0.005 - (dt.datetime.utcnow() - t).total_seconds(), 0)))
