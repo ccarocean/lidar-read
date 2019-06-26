15f9bd70f74a9665e6e7f5fc122788fc2f8aa413
Adam Dodge <adam.dodge@colorado.edu>
2019-06-26 12:50:13 -0600

    Added error handling.

--- lidar/_main.py
+++ lidar/_main.py
@@ -1,4 +1,6 @@
 import datetime as dt
+import sys
+import argparse
 from queue import Queue
 from threading import Thread
 from .led import LED
@@ -8,23 +10,33 @@ from .api import call_send, lidar_packet
 
 def read_key(fname):
     """ Read key for given location to sign data for sending over API link. """
-    with open(fname, 'r') as f:
-        key = f.read()
+    try:
+        with open(fname, 'r') as f:
+            key = f.read()
+    except FileNotFoundError:
+        print('Bad key location. ')
+        sys.exit(0)
     return key
 
 
 def main():
-    led = LED(20)  # initialize LED with pin 15
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--location', type=str, default='harv', help='GPS location. Default is harv.')
+    parser.add_argument('--led', type=int, default=15, help='LED pin. Default is 15. ')
+    args = parser.parse_args()
+
+    led = LED(args.led)  # initialize LED with pin 20
 
     led.switch()
-    loc = 'harv'
+    loc = args.location
     keys = {'harv': read_key('harv.key')}
 
     q = Queue()  # initialize queue for receiving lidar data
     t = Thread(target=collect_data, args=(q,))  # start thread for data collection
     t.start()
 
-    #url = 'https://cods.colorado.edu/api/gpslidar/lidar/' + loc  # Web server location
+    # url = 'https://cods.colorado.edu/api/gpslidar/lidar/' + loc  # Web server location
     url = 'http://127.0.0.1:5000/lidar/' + loc
 
     next_meas, next_t = [], []
@@ -42,7 +54,7 @@ def main():
             next_t, next_meas = [], []
             # Take data until minute is over
             while dt.datetime.utcnow() < end:
-                t, meas = q.get()  # Get data from queue
+                t, meas = q.get(timeout=2)  # Get data from queue
                 if t.second >= prev:
                     meas_vec.append(meas)
                     t_vec.append((t-hour).total_seconds() * 10**6)  # Append microseconds since beginning of the hour
@@ -52,9 +64,9 @@ def main():
                     next_t.append((t-hour).total_seconds() * 10**6)
                     next_meas.append(meas)
                     break
-                if (dt.datetime.now() - led_timer).total_seconds() >= 1:  # Switch lidar state every second
+                if (dt.datetime.utcnow() - led_timer).total_seconds() >= 1:  # Switch lidar state every second
                     led.switch()
-                    led_timer = dt.datetime.now()
+                    led_timer = dt.datetime.utcnow()
                 q.task_done()
 
             # Put data in byte packet
