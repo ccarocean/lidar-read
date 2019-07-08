a6fd543537ce8339a37c4e7f137ab90655858ac5
Adam Dodge <adam.dodge@colorado.edu>
2019-07-08 09:02:29 -0600

    Working on moving to cods.

--- lidar/_main.py
+++ lidar/_main.py
@@ -38,6 +38,7 @@ def main():
     th.start()
 
     url = 'https://cods.colorado.edu/gpslidar/lidar/' + loc  # Web server location
+    print(url)
     # url = 'http://127.0.0.1:5000/lidar/' + loc
 
     led_timer = dt.datetime.utcnow()
