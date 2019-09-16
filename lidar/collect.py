bec088064f232bce7dd91529f8854fcbdeac7b9a
Adam Dodge <adam.dodge@colorado.edu>
2019-09-16 13:38:35 -0600

    Changing options

--- lidar/collect.py
+++ lidar/collect.py
@@ -22,13 +22,15 @@ class Lidar:
         while self.bus.read_byte_data(self.address, 0x01) & 0x01:  # wait for LSB to go low
             pass
         data = self.bus.read_i2c_block_data(self.address, 0x0f, 2)  # Read measurement
-        return (data[0] << 8) + data[1]
+        health = self.bus.read_i2c_block_data(self.address, 0x01, 1)
+        return ((data[0] << 8) + data[1], health[0] & 0x3F)
 
 
 def collect_data(q):
     lid = Lidar(0x62)  # Initialize lidar with address 0x62
     while True:
         t = dt.datetime.utcnow()
-        meas = lid.read_meas()  # Read LiDAR measurement
-        q.put_nowait((t, meas))  # Put time and measurement on queue
-        time.sleep(max((0.005 - (dt.datetime.utcnow() - t).total_seconds(), 0)))  # Sleep to get rate near 200Hz
+        meas, health = lid.read_meas()  # Read LiDAR measurement
+        if (health == 0x3E or health == 0x3A) and meas > 1:
+            q.put_nowait((t, meas))
+            time.sleep(max((0.005 - (dt.datetime.utcnow() - t).total_seconds(), 0)))  # Sleep to get rate near 200Hz
