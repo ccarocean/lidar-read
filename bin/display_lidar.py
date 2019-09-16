0f67d9cb7ea135623d8d4106592a585453a25463
Adam Dodge <adam.dodge@colorado.edu>
2019-09-16 13:23:35 -0600

    Changing options

--- bin/display_lidar.py
+++ bin/display_lidar.py
@@ -13,8 +13,8 @@ class Lidar:
         self.address = address
         if self.bus.read_byte_data(self.address, 0x02) != 0xff:
             self.bus.write_byte_data(self.address, 0x02, 0xff)
-        if self.bus.read_byte_data(self.address, 0x04) != 0x00:
-            self.bus.write_byte_data(self.address, 0x04, 0x00)
+        if self.bus.read_byte_data(self.address, 0x04) != 0x80:
+            self.bus.write_byte_data(self.address, 0x04, 0x80)
         self.bus.write_byte_data(self.address, 0x00, 0x04)
 
     def read_meas(self):
@@ -22,15 +22,16 @@ class Lidar:
         while self.bus.read_byte_data(self.address, 0x01) & 0x01:
             pass
         data = self.bus.read_i2c_block_data(self.address, 0x0f, 2)
-        return (data[0] << 8) + data[1]
+        health = self.bus.read_i2c_block_data(self.address, 0x01, 1)
+        return ((data[0] << 8) + data[1], health & 0x3F)
 
 
 def collect_data(q):
     lid = Lidar(0x62)
     while True:
         t = dt.datetime.utcnow()
-        meas = lid.read_meas()
-        q.put_nowait((t, meas))
+        meas, health = lid.read_meas()
+        q.put_nowait((t, meas, health))
         time.sleep(max((0.005 - (dt.datetime.utcnow() - t).total_seconds(), 0)))
 
 
@@ -39,6 +40,6 @@ t = Thread(target=collect_data, args=(q,))
 t.start()
 
 while True:
-    t, meas = q.get()
-    print('Time:', t, ', Measurement:', meas)
+    t, meas, health = q.get()
+    print('Time:', t, ', Measurement:', meas, 'Health:', health)
     q.task_done()
